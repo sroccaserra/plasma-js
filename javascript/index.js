91b76fae2375ae0b6d29bf6443117a052e8185ee
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-08-11 15:35:39 +0200

    Replace every `var` by `const` or `let`

--- javascript/index.js
+++ javascript/index.js
@@ -11,21 +11,21 @@ window.requestAnimFrame = (function(){
 })();
 
 function drawFrame(context, plasmaMap, colorMap) {
-    var time = new Date().getTime() * 0.003;
+    const time = new Date().getTime() * 0.003;
 
-    var w = context.canvas.width;
-    var h = context.canvas.height;
+    const w = context.canvas.width;
+    const h = context.canvas.height;
     context.imageSmoothingEnabled = false;
     context.mozImageSmoothingEnabled = false
-    var imageData = context.getImageData(0, 0, w, h);
-    var px = imageData.data;
+    const imageData = context.getImageData(0, 0, w, h);
+    const px = imageData.data;
 
-    var kx = w/h;
-    for(var y = 0; y < h; y++) {
-        var yy = y/h - .5;
-        for(var x=0; x < w; x++) {
-            var xx = kx*x/w - kx/2;
-            var v = plasmaMap(xx, yy, time);
+    const kx = w/h;
+    for(let y = 0; y < h; y++) {
+        const yy = y/h - .5;
+        for(let x=0; x < w; x++) {
+            const xx = kx*x/w - kx/2;
+            const v = plasmaMap(xx, yy, time);
             colorMap(px, (y*w + x)*4, v);
         }
     }
@@ -33,14 +33,14 @@ function drawFrame(context, plasmaMap, colorMap) {
 }
 
 function drawStill(canvasId, plasmaMap, colorMap) {
-    var canvas = document.getElementById(canvasId);
-    var context = canvas.getContext( '2d' );
+    const canvas = document.getElementById(canvasId);
+    const context = canvas.getContext( '2d' );
     drawFrame(context, plasmaMap, colorMap);
 }
 
 function drawAnimated(canvasId, plasmaMap, colorMap) {
-    var canvas = document.getElementById(canvasId);
-    var context = canvas.getContext( '2d' );
+    const canvas = document.getElementById(canvasId);
+    const context = canvas.getContext( '2d' );
 
     function animate() {
         drawFrame(context, plasmaMap, colorMap);
@@ -50,12 +50,12 @@ function drawAnimated(canvasId, plasmaMap, colorMap) {
 }
 
 function plasmaFinal(x, y, time) {
-    var v = 0;
+    let v = 0;
     v += Math.sin((x*10+time));
     v += Math.sin((y*10+time)/2.0);
     v += Math.sin((x*10+y*10+time)/2.0);
-    var cx = x + .5 * Math.sin(time/5.0);
-    var cy = y + .5 * Math.cos(time/3.0);
+    const cx = x + .5 * Math.sin(time/5.0);
+    const cy = y + .5 * Math.cos(time/3.0);
     v += Math.sin(Math.sqrt(100*(cx*cx+cy*cy)+1)+time);
     v = v/2.0;
     return v;
@@ -83,7 +83,7 @@ function colorMap3(px, offset, v) {
 }
 
 function colorMap4(px, offset, v) {
-    var c = .5+.5*Math.sin(Math.PI*v*5);
+    const c = .5+.5*Math.sin(Math.PI*v*5);
     px[offset  ] = 255*c;
     px[offset+1] = 255*c;
     px[offset+2] = 255*c;
@@ -91,7 +91,7 @@ function colorMap4(px, offset, v) {
 }
 
 function colorMapGrey(px, offset, v) {
-    var c = 255*(.5+.5*v*.8);
+    const c = 255*(.5+.5*v*.8);
     px[offset  ] = c;
     px[offset+1] = c;
     px[offset+2] = c;
@@ -99,7 +99,7 @@ function colorMapGrey(px, offset, v) {
 }
 
 function colorMapBlue(px, offset, v) {
-    var c = 100*(.5+.5*v*.8)+155;
+    const c = 100*(.5+.5*v*.8)+155;
     if (Math.random() > 1.99) {
         c = 0;
     }
